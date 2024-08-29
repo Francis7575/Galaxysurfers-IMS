b168ba7be6ea5f2e10b867d69f5b4ae0becc0e9c
Francis7575 <francis.albert.2009@gmail.com>
2024-08-29 12:13:54 -0700

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 // CORS options
 const corsOptions = {
