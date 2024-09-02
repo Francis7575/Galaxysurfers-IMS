1823b97a19661a26884cad5e9411b7610d626bf2
Francis7575 <francis.albert.2009@gmail.com>
2024-09-02 16:14:56 -0700

    updated connection.js

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -4,14 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Create a new instance of a pool connection
-const pool = new Pool({
-  user: process.env.DB_USER, // The PostgreSQL username that has access to the database.
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD, // Database password
-  port: process.env.DB_PORT,// The port on which PostgreSQL is listening (5432 is the default port).
-})
+// const pool = new Pool({
+//   user: process.env.DB_USER, // The PostgreSQL username that has access to the database.
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD, // Database password
+//   port: process.env.DB_PORT,// The port on which PostgreSQL is listening (5432 is the default port).
+// })
 
+const pool = new Pool({
+  connectionString: process.env.RENDERDB_INT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // pool.query('SELECT NOW()', (err, res) => {
 //   if (err) {
