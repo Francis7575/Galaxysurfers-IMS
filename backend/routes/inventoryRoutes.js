6b632a9d16c407324b394318d4092a0f03f3ad98
Francis7575 <francis.albert.2009@gmail.com>
2024-09-02 17:13:45 -0700

    updated inventoryRoutes.js

--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { stockIn, stockOut, getStock, getLog } from "../controllers/inventoryController.js"
+import { stockIn, stockOut, getStock } from "../controllers/inventoryController.js"
 const router = express.Router()
 
 
