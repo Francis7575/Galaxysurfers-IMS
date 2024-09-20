97023603c3e8309bf615865f78d0e4e630466e2e
Francis7575 <francis.albert.2009@gmail.com>
2024-09-20 16:30:08 -0700

    updated server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -47,6 +47,7 @@ app.use("/inventory", inventoryRoute);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
+  console.log("Serving index.html for:", req.url);
     res.status(200).sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
