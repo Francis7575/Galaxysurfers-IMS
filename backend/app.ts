15f86f57c70b28f26f309de74c8ad2662847feb2
Francis7575 <francis.albert.2009@gmail.com>
2024-09-24 18:18:31 -0700

    updated app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import * as path from "path";
-// import { Request, Response, NextFunction } from "express";
+import * as path from "path";
+import { Request, Response, NextFunction } from "express";
 import usersRoute from "./routes/usersRoutes";
 import warehousesRoute from "./routes/warehousesRoutes";
 import itemsRoute from "./routes/itemsRoutes";
@@ -25,11 +25,11 @@ const corsOptions = {
 // Use CORS middleware with options
 app.use(cors(corsOptions));
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL!);
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Combine headers
-//   next();
-// });
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL!);
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Combine headers
+  next();
+});
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -41,17 +41,17 @@ app.use("/warehouses", warehousesRoute);
 app.use("/items", itemsRoute);
 app.use("/inventory", inventoryRoute);
 
-// app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-// app.get("*", (req: Request, res: Response) => {
-//   const indexPath = path.join(
-//     __dirname,
-//     "../frontend/dist",
-//     "index.html"
-//   );
-//   console.log("server path:", indexPath);
-//   res.status(200).sendFile(indexPath);
-// });
+app.get("*", (req: Request, res: Response) => {
+  const indexPath = path.join(
+    __dirname,
+    "../frontend/dist",
+    "index.html"
+  );
+  console.log("server path:", indexPath);
+  res.status(200).sendFile(indexPath);
+});
 
 
 export default app
