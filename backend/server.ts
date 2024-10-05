e8146d2cdbc4dfa03172d9f6b78745155255f39a
Francis7575 <francis.albert.2009@gmail.com>
2024-10-04 17:59:12 -0700

    updated server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 app.get("/test", (req: Request, res: Response) => {
-  console.log("hello world")
+  try {
+    console.log("hello world");
+    res.status(200).send("Test route working");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 const PORT = process.env.BACKEND_PORT || 8000;
